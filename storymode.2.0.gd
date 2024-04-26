bcfd66a9b72a6882276446149f69f11b6762316f
Amusementman <harleytoshikian@gmail.com>
2024-04-26 10:51:06 -0700

    holy cow this works!! Deh heck!
    Also to transition to the new ytext is spacebar :p

--- storymode.2.0.gd
+++ storymode.2.0.gd
@@ -2,10 +2,15 @@ extends Node2D
 
 var line = 0
 var show_dialog = false
-var text_speed = 0.5
+var text_speed = 0.05
+var waitTimeBetweenText = 0.25
+
+func _ready():
+	dialog(line)
+	show_dialog = true
 
 func _input(event):
-	if event.is_action("ui_accept"):
+	if event.is_action("ui_accept")&& $Dialogbox.visible_ratio >= 1:
 		advance_text()
 		
 	if event.is_action("ui_cancel"):
@@ -15,6 +20,7 @@ func _input(event):
 #startup point
 
 func advance_text():
+	$Wait.wait_time = waitTimeBetweenText
 	#clear and reset tex
 	clear_text()
 	line +=1
@@ -30,15 +36,16 @@ func _process(delta):
 
 #loop
 func show_text():
-	if show_dialog = true:
-		$Dialogbox.visible_ratio = text_speed
+	if show_dialog == true:
+		$Dialogbox.visible_ratio += text_speed
 		
 
 
 func clear_text():
 	$Dialogbox.text = ""
-	$Dialogbox.visible_characters = -1
+	$Dialogbox.visible_characters = 0
 	$Dialogbox.visible_ratio = 0
+	$Wait.start()
 
 
 
@@ -49,5 +56,10 @@ func dialog(line):
 		1:$Dialogbox.text = "How are you?"
 		2:$Dialogbox.text = "Fine thank you"
 		3:$Dialogbox.text = "Oh my god!"
-		4:$Dialogbox.text = "this is totally not a reference"
+		
 		_:get_tree().quit()
+
+
+func _on_wait_timeout():
+	show_dialog = true
+
